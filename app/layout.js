ce4edc9b20d9893b7aaf361fdd2ed6a2f6cfed2b
Akshaansh1 <srivastava.akshaansh@gmail.com>
2024-06-18 23:48:55 +0530

    First Commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-3xl mx-auto p-4">
+          <Navbar />
+          <div className="mt-8">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
